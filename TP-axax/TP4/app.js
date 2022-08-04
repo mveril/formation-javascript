a96d0526668066d22f0f49052a956de2ff79544e
Mickaël Véril <mika.veril@wanadoo.fr>
2022-08-04 13:57:17 +0200

    Update TP4 UI

--- TP-axax/TP4/app.js
+++ TP-axax/TP4/app.js
@@ -10,14 +10,31 @@ function getWikiSearchUrl(searchitem) {
 
 function createWikiItem(item) {
   const wikiItem = document.createElement("div");
+  wikiItem.classList.add("card","col",
+    "col-xl-2",
+    "col-lg-4",
+    "col-md-6",
+    "col-sm-8",
+    "col-12");
+  const itemTitle = document.createElement("h2");
+  itemTitle.innerText = item.title;
+  const cardHeader=document.createElement("div");
+  cardHeader.classList.add("card-header");
+  cardHeader.appendChild(itemTitle);
+  itemTitle.classList.add("card-title");
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  const snippet=document.createElement("p");
+  snippet.classList.add("card-text");
+  snippet.innerHTML = item.snippet;
   const itemLink = document.createElement("a");
-  itemLink.innerText = item.title;
+  itemLink.classList.add("btn","btn-primary");
+  itemLink.innerText = "Go to Wikipedia";
   const url = new URL("http://fr.wikipedia.org");
   url.searchParams.set("curid",item.pageid)
   itemLink.href = url.toString();
-  const snippet=document.createElement("p");
-  snippet.innerHTML = item.snippet;
-  wikiItem.append(itemLink,snippet);
+  cardBody.append(snippet,itemLink);
+  wikiItem.append(cardHeader,cardBody);
   return wikiItem;
 
 }
@@ -25,7 +42,7 @@ function createWikiItem(item) {
 
 const searchBox = document.querySelector("#search_box");
 const btnSubmit=document.querySelector("#btn_submit");
-const ul_wiki=document.querySelector("#ul_wiki");
+const ul_wiki=document.querySelector("#wiki_cards");
 btnSubmit.addEventListener("click",(event)=>{
   event.preventDefault();
   ul_wiki.innerHTML="";
